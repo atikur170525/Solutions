246c669780b49be2d2b2955b99fa6e15fedd9f4a
MD. ATIKUR RAHMAN <golenur02@gmail.com>
2025-05-17 11:43:04 +0600

    task 1

--- Task1/lcs.js
+++ Task1/lcs.js
@@ -1,10 +1,1 @@
-let a=process.argv.slice(2)
-if(!a.length)return console.log("")
-let s=a[0],r=""
-for(let i=0;i<s.length;++i)
-for(let j=i+1;j<=s.length;++j){
-let sub=s.slice(i,j)
-if(sub.length<=r.length)continue
-if(a.every(x=>x.includes(sub)))r=sub
-}
-console.log(r)
+let a=process.argv.slice(2),r="",s=a[0]||"";for(let i=0;i<s.length;i++)for(let j=i+1;j<=s.length;j++){let t=s.slice(i,j);if(t.length>r.length&&a.every(e=>e.includes(t)))r=t}console.log(r)
